09baa64bdb9425107cb55556732f0a1c30e2eb0e
CesarGentil <cesar.santo01@hotmail.com>
2021-02-25 09:27:36 -0300

    dia 4

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/53150996?s=460&u=13e1c2d6d90ee4f344bd82d7d25150cb26609067&v=4" alt="César augusto"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>César Augusto</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          {level}
         </p>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import '../styles/global.css';
 import {ChallegesProvider} from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
-  
-
   return( 
     <ChallegesProvider>
       <Component {...pageProps} />
